b8948cd6afe836d70034b5d94b8751537e2ea0ca
Duraivelan Palanisamy <p.duraivelanece@gmail.com>
2015-12-12 21:45:10 +0100

    implemented matrix inversion using LAPACK

--- hydrodynamic_diff_calc.cpp
+++ hydrodynamic_diff_calc.cpp
@@ -10,6 +10,30 @@
 # include "defs.h"
 using namespace std;
 
+
+extern "C" {
+    // LU decomoposition of a general matrix
+    void dgetrf_(int* M, int *N, double* A, int* lda, int* IPIV, int* INFO);
+
+    // generate inverse of a matrix given its LU decomposition
+    void dgetri_(int* N, double* A, int* lda, int* IPIV, double* WORK, int* lwork, int* INFO);
+}
+
+void inverse(double* A, int N)
+{
+    int *IPIV = new int[N+1];
+    int LWORK = N*N;
+    double *WORK = new double[LWORK];
+    int INFO;
+
+    dgetrf_(&N,&N,A,&N,IPIV,&INFO);
+    dgetri_(&N,A,&N,IPIV,WORK,&LWORK,&INFO);
+
+    delete IPIV;
+    delete WORK;
+}
+
+
 void createInitialPosition_N_particles(std::string fileName,std::string fileName2, int N, double Lx, double Ly, double Lz) {
 	double x,y,z, vx, vy, vz;
  	srand (time(NULL)); // initialize random seed
@@ -29,22 +53,161 @@ void createInitialPosition_N_particles(std::string fileName,std::string fileName
 }
 int main() {
 // current date/time based on current system
-	time_t now = time(0);
-	struct tm *ltm = localtime(&now);
-	cout << "start time"<< '\t'<< ltm->tm_hour << ":";
-	cout << ltm->tm_min << ":";
-	cout << ltm->tm_sec << endl;
-   
-	int NrParticles = 4; 
-   
-	int mu_6N[6*NrParticles][6*NrParticles];
-   
+   time_t now = time(0);
+   struct tm *ltm = localtime(&now);
+   cout << "start time"<< '\t'<< ltm->tm_hour << ":";
+   cout << ltm->tm_min << ":";
+   cout << ltm->tm_sec << endl;
+         
+int if_create_particles = 0, ifrestart=1;
+         
+double kb=1 , T0=0.3, tauT=0.1;
+double Temp=0;
+double shear_rate = 0; //shear rate
+int ifshear = 0;// set equal to 1 for shear
+std::string dataFileName="1",dataFileName_new="1" ;
+int NrParticles=4;
+double simu_time=dt;
+int step=0, nSteps=10000, frame=10;
+double vel_scale;
+int if_Periodic =1;
+vector<vector<vector<vector<int>>>>  grid(ceil ( box.comp[x] / (r_cut) ),vector<vector<vector<int>>>(ceil ( box.comp[y] / (r_cut) ),vector<vector<int>>(ceil ( box.comp[z] / (r_cut) ),vector<int>(50,0))));
+std::cout<<cellx<<'\t'<<celly<<'\t'<<cellz<<std::endl;
+double  T_Energy, K_Energy, P_Energy, p_energy=0;
+vctr3D dR, dr2;
+double R, r2;
+double dr=0.05; // step size for RDF calculation
+// std::vector<int> RDF((int)  floor(sqrt((Lx/2)*(Lx/2)+(Ly/2)*(Ly/2)+(Lz/2)*(Lz/2)))/dr,0), RDF1((int)  floor(sqrt(Lx*Lx+Ly*Ly))/dr,0);
+double KE_rot=0;
+
+vector<SubData>  particle(NrParticles);
+
+// variables for mobility tensor calculation
+double eta_0=0.01;
+vctr3D Rij;
+double Rij2, Rij2_inv, temp, temp1, temp2, temp3, tau;
+//
+
+if(ifrestart)	{
+std::string fileName=dataFileName+"/End_positions.dat";
+
+//read x,y positions from XY.dat
+std::ifstream dataFile(fileName);
+if(!dataFile.good()) {
+	std::cerr<<"Given file is corrupt /n"<<std::endl;
+}
+else {
+	std::cout<<"Reading X, Y, Z co-ordinates"<<std::endl;
+    std::string line;
+    for (int i=0;i<NrParticles;i++) {
+		std::getline(dataFile,line);
+    	std::istringstream currentLine(line);    
+        currentLine >> particle[i].pos.comp[0];
+        currentLine >> particle[i].pos.comp[1];
+        currentLine >> particle[i].pos.comp[2];
+		currentLine >> particle[i].radius;
+		cout<<particle[i].radius<<endl;
+    }
+}	
+	fileName=dataFileName+"/Velocities.dat";
+	std::ifstream dataFile1(fileName);
+
+	if(!dataFile1.good()) {
+	std::cerr<<"Given file is corrupt /n"<<std::endl;
+}
+else {
+	std::cout<<"Reading Vx, Vy, Vz Velocities"<<std::endl;
+    std::string line;
+     
+    for (int i=0;i<NrParticles;i++) {
+		std::getline(dataFile1,line);
+    	std::istringstream currentLine(line);
+        currentLine >> particle[i].vel.comp[0];
+        currentLine >> particle[i].vel.comp[1];
+        currentLine >> particle[i].vel.comp[2];
+
+    }
+}	
+} else {
+
+	std::string fileName="../XYZ.dat";
+	std::string fileName2="../Initial_Velocities.dat";
+
+if (if_create_particles) {
+createInitialPosition_N_particles(fileName,fileName2,NrParticles,Lx,Ly,Lz);
+}
+//read x,y positions from XY.dat
+std::ifstream dataFile(fileName);
+std::ifstream dataFile2(fileName2);
+
+if(!dataFile.good() && !dataFile2.good() ) {
+	std::cerr<<"Given file is corrupt /n"<<std::endl;
+}
+else {
+	std::cout<<"Reading X, Y, Z co-ordinates"<<std::endl;
+    std::string line;
+    std::string line1;
+
+
+    for (int i=0;i<NrParticles;i++) {
+		std::getline(dataFile,line);
+    	std::istringstream currentLine(line);
+        currentLine >> particle[i].pos.comp[0];
+        currentLine >> particle[i].pos.comp[1];
+        currentLine >> particle[i].pos.comp[2];
+        currentLine >> particle[i].radius;
+		std::getline(dataFile2,line1);
+    	std::istringstream currentLine1(line1);
+        currentLine1 >> particle[i].vel.comp[0];
+        currentLine1 >> particle[i].vel.comp[1];
+        currentLine1 >> particle[i].vel.comp[2];  
+        Temp+=0.5*m*(particle[i].vel.comp[0]*particle[i].vel.comp[0]
+				   + particle[i].vel.comp[1]*particle[i].vel.comp[1]
+				   + particle[i].vel.comp[2]*particle[i].vel.comp[2]);
+              
+    }
+    	Temp=(Temp)/(1.5*NrParticles*kb);
+		vel_scale = sqrt(T0/Temp);
+		std::cout<<Temp<<'\t'<<vel_scale<<std::endl;
+
+}
+
+}		
+//delete all files before writing data
+
+// following snippet taken from stakcflow link  http://stackoverflow.com/questions/11007494/how-to-delete-all-files-in-a-folder-but-not-delete-the-folder-c-linux
+if (ifrestart) {
+dataFileName=dataFileName_new;
+}
+const char *dataFileNamePointer = dataFileName.c_str();  // covnert the datafilename to a char pointer ans pass it to the snippet below which delete all files in that folder before running the simulation
+if (!ifrestart) {
+struct dirent *next_file;
+DIR *theFolder;
+char filepath[256];
+theFolder = opendir(dataFileNamePointer);
+while (( next_file = readdir(theFolder)) )
+	{
+    // build the full path for each file in the folder
+    sprintf(filepath, "%s/%s",dataFileNamePointer, next_file->d_name);
+    remove(filepath);
+    }
+//
+}
+      
 	mtrx3D Mobility_Tnsr_tt;
 	mtrx3D Mobility_Tnsr_tr;
 	mtrx3D Mobility_Tnsr_rt;
 	mtrx3D Mobility_Tnsr_rr;
    
+   double mu_6N[36*NrParticles*NrParticles] ;
    
+   for (int i=0; i<36*NrParticles; i++)
+		{
+			mu_6N[i] = 0; 
+		}        
+		
+std::ofstream outFile1(dataFileName+"/data.dat");
+		
 for (int i=0; i<NrParticles; i++)
 	{
 		for (int j=0; j<NrParticles; j++)
@@ -78,7 +241,7 @@ for (int i=0; i<NrParticles; i++)
 				Mobility_Tnsr_rr	=		(Unit_diag*(-1.0) + (Rij^Rij)*Rij2_inv*3.0)*temp3 ;
 				
 				Mobility_Tnsr_rt	=	  	epsilon_rij*(-2.0)*temp3;
-				Mobility_Tnsr_tr	= 	Mobility_Tnsr_rt[i][j];
+				Mobility_Tnsr_tr	= 	Mobility_Tnsr_rt;
 												
 			 } 
 			
@@ -86,12 +249,19 @@ for (int i=0; i<NrParticles; i++)
 				{
 					for (int l=0; l<3; l++)
 						{
-							mu_6N[k+3*i][l+3*i] 								=	 Mobility_Tnsr_tt.comp[k][l];
-							mu_6N[k+3*i][l+3*i+3*NrParticles] 					=	 Mobility_Tnsr_tr.comp[k][l];
-							mu_6N[k+3*i+3*NrParticles][l+3*i] 					=	 Mobility_Tnsr_rt.comp[k][l];
-							mu_6N[k+3*i+3*NrParticles][l+3*i+3*NrParticles] 	=	 Mobility_Tnsr_rr.comp[k][l];
+							mu_6N[l+3*k+9*j+i*9*NrParticles] 								=	 Mobility_Tnsr_tt.comp[k][l];
+							mu_6N[l+3*k+9*j+i*9*NrParticles+ 9*NrParticles*NrParticles] 	=	 Mobility_Tnsr_tr.comp[k][l];
+							mu_6N[l+3*k+9*j+i*9*NrParticles+18*NrParticles*NrParticles] 	=	 Mobility_Tnsr_rt.comp[k][l];
+							mu_6N[l+3*k+9*j+i*9*NrParticles+27*NrParticles*NrParticles] 	=	 Mobility_Tnsr_rr.comp[k][l];
 						}
 				}	
 				
 			}	
-	}	
+					
+	for (int i=0; i<36*NrParticles; i++)
+		{
+			outFile1 << mu_6N[i] << endl;
+		} 
+	}
+}
+		
---
+++ test.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <random>
+
+extern "C" {
+    // LU decomoposition of a general matrix
+    void dgetrf_(int* M, int *N, double* A, int* lda, int* IPIV, int* INFO);
+
+    // generate inverse of a matrix given its LU decomposition
+    void dgetri_(int* N, double* A, int* lda, int* IPIV, double* WORK, int* lwork, int* INFO);
+}
+
+void inverse(double* A, int N)
+{
+    int *IPIV = new int[N+1];
+    int LWORK = N*N;
+    double *WORK = new double[LWORK];
+    int INFO;
+
+    dgetrf_(&N,&N,A,&N,IPIV,&INFO);
+    dgetri_(&N,A,&N,IPIV,WORK,&LWORK,&INFO);
+
+    delete IPIV;
+    delete WORK;
+}
+
+using namespace std;
+int main(){
+
+    double A [10000*10000] ;
+    
+    for(int i=0;i<100000000;i++) {
+		A[i]= rand();
+	}
+
+    inverse(A, 10000);
+
+    cout<< A[0]<<'\t'<<A[1]<<endl;
+    cout<<A[2]<<'\t'<<A[3]<<endl;
+
+    return 0;
+}
